9359e341225c25ea05c3ed7afb29c717c4bf2783
Endy Muhardin <endy.muhardin@gmail.com>
2021-09-22 18:25:35 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/belajartimezone/BelajarTimezoneApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.belajartimezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarTimezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarTimezoneApplication.class, args);
+	}
+
+}
